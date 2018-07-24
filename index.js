6c0c10f3ec1201bcc562980de91b4634b80ee3d5
Anderson <primataxdeus@gmail.com>
2018-07-24 12:35:21 -0300

    Setting up Nunjucks

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
 
 const app = express();
+
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+app.set('view engine', 'njk');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Running');
+  res.render('index');
 });
 
 app.listen('3000');
