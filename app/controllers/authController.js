1e73c503907e878d73670529bc06dc90a8236306
Anderson <primataxdeus@gmail.com>
2018-07-25 16:14:50 -0300

    Added Users Regiser router, and bcrpyptjs :sparkles:

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,8 +1,26 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+
 module.exports = {
+
   signin(req, res) {
     return res.render('auth/signin');
   },
+
   signup(req, res) {
     return res.render('auth/signup');
+  },
+
+  async register(req, res) {
+    const { email } = req.body;
+
+    if (await User.findOne({ where: { email } })) {
+      return res.redirect('back');
+    }
+
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    await User.create({ ...req.body, password });
+    return res.redirect('/');
   },
 };
--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,6 @@ const routes = express.Router();
 routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
 
+routes.post('/register', authController.register);
+
 module.exports = routes;
