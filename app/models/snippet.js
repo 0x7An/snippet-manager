f0c1acc7118763f1e11b3bd373a8b82af8651f27
Anderson <primataxdeus@gmail.com>
2018-07-28 20:10:55 -0300

    Categories controller, Categories View, and listing Snippets

--- app/models/snippet.js
+++ app/models/snippet.js
@@ -2,6 +2,14 @@ module.exports = (sequelize, DataTypes) => {
   const Snippet = sequelize.define('Snippet', {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
+  }, {
+    getterMethods: {
+      exercpt() {
+        return this.content.length > 120
+          ? `${this.content.substring(0, this.content.lastndexOf(' ', 120))}...`
+          : this.content;
+      },
+    },
   });
 
   Snippet.associate = (models) => {
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
+const categoryController = require('./controllers/categoryController');
+
 const authMidleware = require('./midlewares/auth');
 const guestMidleware = require('./midlewares/guest');
 
@@ -12,6 +14,7 @@ routes.use((req, res, next) => {
   next();
 });
 
+// Auth
 routes.get('/', guestMidleware, authController.signin);
 routes.get('/signup', guestMidleware, authController.signup);
 routes.get('/signout', authController.signout);
@@ -19,8 +22,13 @@ routes.get('/signout', authController.signout);
 routes.post('/register', authController.register);
 routes.post('/authenticate', authController.authenticate);
 
+// dashboard
 routes.use('/app', authMidleware);
 routes.get('/app/dashboard', dashboardController.index);
+
+// Categories
+routes.use('/app/categories/:id', categoryController.show);
+routes.use('/app/categories/create', categoryController.store);
 
 routes.use((req, res) => res.render('errors/index'));
 
